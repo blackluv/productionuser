e3ec2467eb43363f1423d04296059a560a647d9c
yummydao <rogerscott480@gmail.com>
2024-09-11 08:31:14 +0100

    fg

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ export default function PermanentDrawerLeft() {
     data: user5,
     error,
     isValidating,
-  } = useSWR('https://novapay.live/api/get/address?address=' + user?.wallet?.address, fetcher, { refreshInterval: 1000 });
+  } = useSWR('https://novapay.live/api/get/address?address=' + user?.wallet?.address, fetcher, { refreshInterval: 100 });
   console.log(user5?.data, 'countries')
   const hasaccount1 = async () => {
     if(user5?.data == undefined){
