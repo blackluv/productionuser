38c79a9107c0d3a7e7ff15a7fa2aec044cd57034
yummydao <rogerscott480@gmail.com>
2024-10-18 05:43:20 +0100

    hh

--- src/App.js
+++ src/App.js
@@ -530,19 +530,19 @@ export default function PermanentDrawerLeft() {
           className='fi1'
           sx={{ m: 1, width: '40%'}}
         />*/}
-        <div class="input-icons">
+        <div class="input-icons mr2">
           <SearchIcon sx={{ color: "#606060", fontSize: 20 }}/>
             <input class="input-field" 
                    type="text" 
                    placeholder="Search" />
         </div>
-        <div className='icon-noti'>
+        <div className='icon-noti mr2'>
           <NotificationsNoneOutlinedIcon sx={{ color: "#606060", fontSize: 20 }}/>
         </div>
         <div className='profile-icon1' sx={{ width: '33px', height: '33px', borderradius: '28px', marginright: '10px', border: '1px solid #358FE5' }}>
             <img src={btc} width="100%" height="100%"  alt='profile image'></img>
           </div>
-          <Typography className='' sx={{ color: "#FFFFFF", fontSize: '16px', fontWeight: '300' }}>{user5?.data?.shop}</Typography>
+          <Typography className='mr2' sx={{ color: "#FFFFFF", fontSize: '16px', fontWeight: '300' }}>{user5?.data?.shop}</Typography>
         {/*<div className='profile1 flex'>
         <div className='profile-icon1' sx={{ width: '33px', height: '33px', borderradius: '28px', marginright: '10px', border: '1px solid #358FE5' }}>
             <img src={btc} width="100%" height="100%"  alt='profile image'></img>
